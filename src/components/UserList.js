1323b902176dbfbb1e951cd97c116445a01eb5e6
Dan Esparza <esparza.dan@gmail.com>
2018-11-09 14:58:24 -0500

    Add semicolon after import

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -4,7 +4,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-import MaterialTable from 'material-table'
+import MaterialTable from 'material-table';
 
 const styles = theme => ({
   button: {
