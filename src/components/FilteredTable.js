3dc68a68e1e66c8a1b4247eefd40c2f016f48533
Dan Esparza <esparza.dan@gmail.com>
2018-11-14 15:33:28 -0500

    Layout and grid

--- src/components/FilteredTable.js
+++ src/components/FilteredTable.js
@@ -20,11 +20,23 @@ import Tooltip from '@material-ui/core/Tooltip';
 import DeleteIcon from '@material-ui/icons/Delete';
 import { lighten } from '@material-ui/core/styles/colorManipulator';
 
-let counter = 0;
-function createData(name, calories, fat, carbs, protein) {
-  counter += 1;
-  return { id: counter, name, calories, fat, carbs, protein };
-}
+
+/**
+ * Props:
+ * 
+ * rows - an array of row data that defines what to show in the table. Example:
+   const rows = [
+  { id: 'name', numeric: false, disablePadding: true, label: 'Name' },
+  { id: 'calories', numeric: true, disablePadding: false, label: 'Calories' },
+  { id: 'fat', numeric: true, disablePadding: false, label: 'Fat (g)' },
+  { id: 'carbs', numeric: true, disablePadding: false, label: 'Carbs (g)' },
+  { id: 'protein', numeric: true, disablePadding: false, label: 'Protein (g)' },
+    ];
+ * 
+ * data - an array of data to display.  Each item that should be displayed should be defined
+ * in the 'rows' prop
+ * 
+ */
 
 function desc(a, b, orderBy) {
   if (b[orderBy] < a[orderBy]) {
@@ -204,19 +216,19 @@ class FilteredTable extends React.Component {
     orderBy: 'calories',
     selected: [],
     data: [
-      createData('Cupcake', 305, 3.7, 67, 4.3),
-      createData('Donut', 452, 25.0, 51, 4.9),
-      createData('Eclair', 262, 16.0, 24, 6.0),
-      createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-      createData('Gingerbread', 356, 16.0, 49, 3.9),
-      createData('Honeycomb', 408, 3.2, 87, 6.5),
-      createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-      createData('Jelly Bean', 375, 0.0, 94, 0.0),
-      createData('KitKat', 518, 26.0, 65, 7.0),
-      createData('Lollipop', 392, 0.2, 98, 0.0),
-      createData('Marshmallow', 318, 0, 81, 2.0),
-      createData('Nougat', 360, 19.0, 9, 37.0),
-      createData('Oreo', 437, 18.0, 63, 4.0),
+      {"id": "Cupcake", "name": "Cupcake", "calories": 305, "fat": 3.7, "carbs": 67, "protein": 4.3},
+      {"id": "Donut", "name": "Donut", "calories": 452, "fat": 25.0, "carbs": 51, "protein": 4.9},
+      {"id": "Eclair", "name": "Eclair", "calories": 262, "fat": 16.0, "carbs": 24, "protein": 6.0},
+      {"id": "Frozen yoghurt", "name": "Frozen yoghurt", "calories": 159, "fat": 6.0, "carbs": 24, "protein": 4.0},
+      {"id": "Gingerbread", "name": "Gingerbread", "calories": 356, "fat": 16.0, "carbs": 49, "protein": 3.9},
+      {"id": "Honeycomb", "name": "Honeycomb", "calories": 408, "fat": 3.2, "carbs": 87, "protein": 6.5},
+      {"id": "Ice cream sandwich", "name": "Ice cream sandwich", "calories": 237, "fat": 9.0, "carbs": 37, "protein": 4.3},
+      {"id": "Jelly Bean", "name": "Jelly Bean", "calories": 375, "fat": 0.0, "carbs": 94, "protein": 0.0},
+      {"id": "KitKat", "name": "KitKat", "calories": 518, "fat": 26.0, "carbs": 65, "protein": 7.0},
+      {"id": "Lollipop", "name": "Lollipop", "calories": 392, "fat": 0.2, "carbs": 98, "protein": 0.0},
+      {"id": "Marshmallow", "name": "Marshmallow", "calories": 318, "fat": 0, "carbs": 81, "protein": 2.0},
+      {"id": "Nougat", "name": "Nougat", "calories": 360, "fat": 19.0, "carbs": 9, "protein": 37.0},
+      {"id": "Oreo", "name": "Oreo", "calories": 437, "fat": 18.0, "carbs": 63, "protein": 4.0},
     ],
     page: 0,
     rowsPerPage: 5,
--- src/components/users/UserList.js
+++ src/components/users/UserList.js
@@ -4,13 +4,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import Refresh from '@material-ui/icons/Refresh';
 
 import FilteredTable from '../FilteredTable';
 
 const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
   button: {
     margin: theme.spacing.unit,
-  }, 
+  },  
 });
 
 function UserList(props){
@@ -18,15 +26,21 @@ function UserList(props){
   const { classes } = props;
 
   return (
-    <div>
-        <Button size="small" variant="contained" color="primary" className={classes.button}>
-          Add user
-        </Button>
-        <Button size="small" variant="contained" color="secondary" className={classes.button}>
-          Delete user
-        </Button>
+    <div className={classes.root}>    
+      
+      <Button size="small" variant="contained" color="primary" className={classes.button}>
+        Add user
+      </Button>
+      <Button size="small" variant="contained" color="secondary" className={classes.button}>
+        Delete user
+      </Button>
 
-        <FilteredTable />
+      <IconButton className={classes.button} aria-label="Delete">
+        <Refresh />
+      </IconButton>
+    
+      <FilteredTable />
+        
     </div>
   );
 }
