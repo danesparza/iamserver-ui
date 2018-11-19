e404c88761969fd24b94f8d1ad7dd3a2591ca562
Dan Esparza <esparza.dan@gmail.com>
2018-11-19 15:35:21 -0500

    Figured out flexbox action bar alignment

--- src/components/users/UserList.js
+++ src/components/users/UserList.js
@@ -1,24 +1,23 @@
 //  React
 import React from 'react';
 
+import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import Refresh from '@material-ui/icons/Refresh';
+import Grid from '@material-ui/core/Grid';
 
 import FilteredTable from '../FilteredTable';
 
 const styles = theme => ({
   root: {
     flexGrow: 1,
-  },
-  grow: {
-    flexGrow: 1,
-  },
+  },  
   button: {
     margin: theme.spacing.unit,
-  },  
+  },
 });
 
 function UserList(props){
@@ -26,22 +25,29 @@ function UserList(props){
   const { classes } = props;
 
   return (
-    <div className={classes.root}>    
-      
-      <Button size="small" variant="contained" color="primary" className={classes.button}>
-        Add user
-      </Button>
-      <Button size="small" variant="contained" color="secondary" className={classes.button}>
-        Delete user
-      </Button>
-
-      <IconButton className={classes.button} aria-label="Delete">
-        <Refresh />
-      </IconButton>
-    
-      <FilteredTable />
+    <div>
+      <Grid container className={classes.root} justify="space-between">    
         
+        <Grid item>
+          <Button size="small" variant="contained" color="primary" className={classes.button}>
+            Add user
+          </Button>
+          <Button size="small" variant="contained" color="secondary" className={classNames(classes.button, classes.start )}>
+            Delete user
+          </Button>
+        </Grid>
+
+        <Grid item>
+          <IconButton className={classes.button} aria-label="Refresh">
+            <Refresh />
+          </IconButton>
+        </Grid>            
+      </Grid>
+
+      <FilteredTable />        
+    
     </div>
+
   );
 }
 
