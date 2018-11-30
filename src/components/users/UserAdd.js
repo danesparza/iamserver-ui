96acf8acf8327fcaf69792e97aa2891cbe5e2d2d
Dan Esparza <esparza.dan@gmail.com>
2018-11-30 14:24:23 -0500

    Add basic stepper framework for user add

--- src/components/users/UserAdd.js
+++ src/components/users/UserAdd.js
@@ -1,16 +1,142 @@
 //  React
 import React, { Component } from 'react';
 
-class UserAdd extends Component {  
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
-    render() {
+//  Step components
+import BasicInfo from './BasicInfo';
+import AddPermissions from './AddPermissions';
+import AddReview from './AddReview';
 
-      return (
-        <div>
-          Yep.  Adding users here          
-        </div>
-      );
-    }
+const styles = theme => ({
+  root: {
+    width: '90%',
+  },
+  button: {
+    marginRight: theme.spacing.unit,
+  },
+  instructions: {
+    marginTop: theme.spacing.unit,
+    marginBottom: theme.spacing.unit,
+  },
+});
+
+function getSteps() {
+  return ['Basic information', 'Permissions', 'Review'];
 }
 
-export default UserAdd;
+function getStepContent(step) {
+  switch (step) {
+    case 0:
+      return <BasicInfo />;
+    case 1:
+      return <AddPermissions />;
+    case 2:
+      return <AddReview />;
+    default:
+      return 'Unknown step';
+  }
+}
+
+class UserAdd extends Component {
+  state = {
+    activeStep: 0,
+    skipped: new Set(),
+  };
+
+  handleNext = () => {
+    const { activeStep } = this.state;
+    let { skipped } = this.state;
+    if (this.isStepSkipped(activeStep)) {
+      skipped = new Set(skipped.values());
+      skipped.delete(activeStep);
+    }
+    this.setState({
+      activeStep: activeStep + 1,
+      skipped,
+    });
+  };
+
+  handleBack = () => {
+    this.setState(state => ({
+      activeStep: state.activeStep - 1,
+    }));
+  };
+
+  handleReset = () => {
+    this.setState({
+      activeStep: 0,
+    });
+  };
+
+  isStepSkipped(step) {
+    return this.state.skipped.has(step);
+  }
+
+  render() {
+    const { classes } = this.props;
+    const steps = getSteps();
+    const { activeStep } = this.state;
+
+    return (
+      <div className={classes.root}>
+        <Stepper activeStep={activeStep}>
+          {steps.map((label, index) => {
+            const props = {};
+            const labelProps = {};
+            return (
+              <Step key={label} {...props}>
+                <StepLabel {...labelProps}>{label}</StepLabel>
+              </Step>
+            );
+          })}
+        </Stepper>
+        <div>
+          {activeStep === steps.length ? (
+            <div>
+              <Typography className={classes.instructions}>
+                All steps completed - you&apos;re finished
+              </Typography>
+              <Button onClick={this.handleReset} className={classes.button}>
+                Reset
+              </Button>
+            </div>
+          ) : (
+            <div>
+              <Typography className={classes.instructions}>{getStepContent(activeStep)}</Typography>
+              <div>
+                <Button
+                  disabled={activeStep === 0}
+                  onClick={this.handleBack}
+                  className={classes.button}
+                >
+                  Back
+                </Button>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={this.handleNext}
+                  className={classes.button}
+                >
+                  {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+                </Button>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
+
+UserAdd.propTypes = {
+  classes: PropTypes.object,
+};
+
+export default withStyles(styles)(UserAdd);
