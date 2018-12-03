90b471505a2673d6d4736a5960d252040d0cb686
Dan Esparza <esparza.dan@gmail.com>
2018-12-03 14:41:56 -0500

    Add display idea for MFA setup

--- src/components/users/BasicInfo.js
+++ src/components/users/BasicInfo.js
@@ -1,16 +1,111 @@
 //  React
 import React, { Component } from 'react';
 
+import { withStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    flexBasis: '33.33%',
+    flexShrink: 0,
+  },
+  secondaryHeading: {
+    fontSize: theme.typography.pxToRem(15),
+    color: theme.palette.text.secondary,
+  },
+});
+
 class BasicInfo extends Component {  
 
+    state = {
+      expanded: null,
+    };
+
+    handleChange = panel => (event, expanded) => {
+      this.setState({
+        expanded: expanded ? panel : false,
+      });
+    };
+
     render() {
+      const { classes } = this.props;
+      const { expanded } = this.state;
 
       return (
-        <div>
-          Includes name, password, enabled, TOTP (MFA), creation/update/delete history          
-        </div>
+        <React.Fragment>
+          <Typography variant="h6" gutterBottom>
+            Basic information
+          </Typography>
+          <Grid container spacing={24}>
+            <Grid item xs={12}>
+              <TextField
+                required
+                id="name"
+                name="name"
+                label="Login name"
+                fullWidth
+                autoComplete="fname"                
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                required
+                type="password"
+                id="password"
+                name="password"
+                label="Password"
+                fullWidth
+                autoComplete="pass"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                required
+                type="password"
+                id="verifypassword"
+                name="verifypassword"
+                label="Verify password"
+                fullWidth
+                autoComplete="verifypass"
+              />
+            </Grid>
+
+            <Grid item xs={12}>
+              <ExpansionPanel expanded={expanded === 'panel1'} onChange={this.handleChange('panel1')}>
+                <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                  <Typography className={classes.heading}>MFA settings</Typography>
+                  <Typography className={classes.secondaryHeading}>MFA not enabled</Typography>
+                </ExpansionPanelSummary>
+                <ExpansionPanelDetails>
+                  <Typography>
+                    Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat. Aliquam eget
+                    maximus est, id dignissim quam.
+                  </Typography>
+                </ExpansionPanelDetails>
+              </ExpansionPanel>
+            </Grid>
+
+          </Grid>                            
+
+      </React.Fragment>
       );
     }
 }
 
-export default BasicInfo;
+BasicInfo.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(BasicInfo);
