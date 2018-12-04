fc0eca5fbe0f590b8f45bb5c3bcd7d81c30588cd
Dan Esparza <esparza.dan@gmail.com>
2018-12-04 09:28:17 -0500

    Update BasicInfo.js

--- src/components/users/BasicInfo.js
+++ src/components/users/BasicInfo.js
@@ -7,95 +7,65 @@ import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 
-import ExpansionPanel from '@material-ui/core/ExpansionPanel';
-import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
-import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+function TabContainer(props) {
+  return (
+    <Typography component="div" style={{ padding: 8 * 3 }}>
+      {props.children}
+    </Typography>
+  );
+}
+
+TabContainer.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 
 const styles = theme => ({
   root: {
-    width: '100%',
-  },
-  heading: {
-    fontSize: theme.typography.pxToRem(15),
-    flexBasis: '33.33%',
-    flexShrink: 0,
-  },
-  secondaryHeading: {
-    fontSize: theme.typography.pxToRem(15),
-    color: theme.palette.text.secondary,
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
   },
 });
 
 class BasicInfo extends Component {  
 
     state = {
-      expanded: null,
+      value: 0,
     };
 
-    handleChange = panel => (event, expanded) => {
-      this.setState({
-        expanded: expanded ? panel : false,
-      });
+    handleChange = (event, value) => {
+      this.setState({ value });
     };
 
     render() {
-      const { classes } = this.props;
-      const { expanded } = this.state;
 
       return (
         <React.Fragment>
+          
           <Typography variant="h6" gutterBottom>
-            Basic information
+            Set user details
           </Typography>
+
           <Grid container spacing={24}>
             <Grid item xs={12}>
               <TextField
                 required
                 id="name"
                 name="name"
-                label="Login name"
-                fullWidth
+                label="Login name"                
                 autoComplete="fname"                
               />
             </Grid>
-            <Grid item xs={12} sm={6}>
+
+            <Grid item xs={12}>
               <TextField
                 required
                 type="password"
                 id="password"
                 name="password"
                 label="Password"
-                fullWidth
                 autoComplete="pass"
               />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                required
-                type="password"
-                id="verifypassword"
-                name="verifypassword"
-                label="Verify password"
-                fullWidth
-                autoComplete="verifypass"
-              />
-            </Grid>
-
-            <Grid item xs={12}>
-              <ExpansionPanel expanded={expanded === 'panel1'} onChange={this.handleChange('panel1')}>
-                <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-                  <Typography className={classes.heading}>MFA settings</Typography>
-                  <Typography className={classes.secondaryHeading}>MFA not enabled</Typography>
-                </ExpansionPanelSummary>
-                <ExpansionPanelDetails>
-                  <Typography>
-                    Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat. Aliquam eget
-                    maximus est, id dignissim quam.
-                  </Typography>
-                </ExpansionPanelDetails>
-              </ExpansionPanel>
-            </Grid>
+            </Grid>            
 
           </Grid>                            
 
